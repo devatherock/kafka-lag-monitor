a06cc7a814ab9c421050840dcae5cee89bb0340c
devatherock <devatherock@gmail.com>
2024-01-19 18:52:48 -0600

    test: Added unit test with kafka, to capture reflection config
    Closes #172

---
+++ unit-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+additional_gradle_args=$1
+
+docker network create ci-network || true
+export DOCKER_NETWORK_IP=$(docker network inspect ci-network -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}')
+docker compose up -d
+./gradlew test -Dgraalvm=true ${additional_gradle_args}
+exit_code=$?
+
+docker compose down
+exit $exit_code
