45c115a3d9a61e23ce27118f6a02d65ba89ede91
devatherock <devatherock@gmail.com>
2024-01-23 21:15:43 -0600

    test: Used an influxdb container for tests

--- src/test/groovy/io/github/devatherock/scheduler/KafkaLagCollectorBaseSpec.groovy
+++ src/test/groovy/io/github/devatherock/scheduler/KafkaLagCollectorBaseSpec.groovy
@@ -7,7 +7,11 @@ import javax.inject.Inject
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.influxdb.InfluxDB
+import org.influxdb.InfluxDBFactory
+import org.influxdb.dto.Query
 
+import io.micronaut.context.env.Environment
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import spock.lang.Specification
@@ -21,12 +25,17 @@ abstract class KafkaLagCollectorBaseSpec extends Specification {
     @Client('${test.server.url}')
     HttpClient httpClient
 
+    @Inject
+    Environment environment
+
     KafkaProducer producer
     KafkaConsumer consumer
     String topic = 'test-topic'
-    PollingConditions conditions = new PollingConditions(timeout : 15, initialDelay : 1, delay : 1)
+    PollingConditions conditions = new PollingConditions(timeout: 15, initialDelay: 1, delay: 1)
+    InfluxDB influxDB
 
     void setup() {
+        // Initialize kafka producer and consumer
         def commonConfig = [
                 'bootstrap.servers': "${System.env.DOCKER_NETWORK_IP ?: 'localhost'}:9092".toString(),
         ]
@@ -42,6 +51,12 @@ abstract class KafkaLagCollectorBaseSpec extends Specification {
         consumerConfig['value.deserializer'] = 'org.apache.kafka.common.serialization.StringDeserializer'
         consumerConfig['auto.offset.reset'] = 'earliest'
         consumer = new KafkaConsumer(consumerConfig)
+
+        // Initialize influx client
+        influxDB = InfluxDBFactory.connect(
+                environment.getProperty('micronaut.metrics.export.influx.uri', String).get(),
+        )
+        influxDB.database = 'mydb'
     }
 
     void 'test prometheus metrics'() {
@@ -53,16 +68,24 @@ abstract class KafkaLagCollectorBaseSpec extends Specification {
         consumer.commitSync()
         consumer.unsubscribe()
 
-        and: 'produce 2 more messages'
+        when: 'produce 2 more messages'
         producer.send(new ProducerRecord(topic, 'key-2', 'message-2'))
         producer.send(new ProducerRecord(topic, 'key-3', 'message-3'))
 
-        expect:
+        then:
         conditions.eventually {
             httpClient.toBlocking().retrieve('/prometheus').contains('''
             kafka_consumer_lag_max{cluster_name="test-cluster",group="test-consumer",partition="0",topic="test-topic",} 2.0
         '''.trim()
             )
+        }
+
+        and:
+        conditions.eventually {
+            influxDB.query(new Query(
+                    '''select last(upper) from kafka_consumer_lag where "cluster_name" = 'test-cluster' ''' +
+                            '''and "group" = 'test-consumer' and "topic" = 'test-topic' and "partition" = '0' '''.trim()
+            )).results[0].series[0].values[0][1] == 2
         }
     }
 }
